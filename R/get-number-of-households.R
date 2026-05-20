17d3de1f4113d5ef6404dd3809ac9a2508e869af
Topi-Matti Heikkola <topi-matti@heikkola.fi>
2026-05-20 13:30:24 +0300

    consumption income and prerequisites to new target structure

--- R/get-number-of-households.R
+++ R/get-number-of-households.R
@@ -1,20 +1,51 @@
-get_number_of_households <- function(geo, year) {
-  res <- eurostat::get_eurostat(
+pull_raw_data_n_households <- function(geo, year) {
+
+  source("R/fingreen-r-utils.R")
+
+  global_params <- config::get(file = "global-params.yml")
+  base_year <- global_params$base_year
+  geo <- global_params$geo
+
+  working_directory <- getwd()
+
+  raw_data_dir <- paste0(working_directory, "/raw-data/shared/")
+  create_dir_if_not_exists(raw_data_dir, "raw data")
+
+  n_households <- eurostat::get_eurostat(
     "lfst_hhnhtych",
     time_format = "num",
     filters = list(
       geo = geo,
-      time = year,
+      time = base_year,
       agechild = "TOTAL",
       n_child = "TOTAL",
       phhcomp = "TOTAL"
     )
-  ) |> 
+  )
+  n_households_schema <- structure(
+    list(
+      column_name = c('freq', 'agechild', 'n_child', 'phhcomp', 'unit', 'geo', 'time', 'values'),
+      column_type = c('character', 'character', 'character', 'character', 'character', 'character', 'numeric', 'numeric')
+    ),
+    class = 'data.frame',
+    row.names = c('freq', 'agechild', 'n_child', 'phhcomp', 'unit', 'geo', 'time', 'values')
+  )
+  validate_schema(n_households, n_households_schema, "n_households")
+  
+  output_path <- paste0(raw_data_dir, "n-households.ods")
+  
+  readODS::write_ods(n_households, path = output_path, sheet = "n_households")
+
+  return(output_path)
+}
+
+get_number_of_households <- function(raw_data_path) {
+   n_households <- readODS::read_ods(raw_data_path, sheet = "n_households") |> 
     dplyr::transmute(
       geo = geo,
       year = as.integer(time),
       n_households = as.integer(values * 1e3)
     )
 
-  return(res)
+  return(n_households)
 }
